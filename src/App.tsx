8ec4de217d7c4edc07741c1f9184fa7e6527f465
hhormax <hormax14@gmail.com>
2023-12-10 01:34:52 +0300

    some css

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ function App() {
   const [value, setValue] = useState(() => 0)
 
   return (
-    <div className="counter">
-      <div className="value">{value}</div>
-      <div className="buttons">
-        <button onClick={() => {setValue(value + 1)}} className="increase">+</button>
-        <button onClick={() => {setValue(0)}} className="reset">Reset</button>
-        <button onClick={() => {setValue(value - 1)}} className="decrease">-</button>
+    <div className="wrapper">
+      <div className="counter">
+        <div className="value">{value}</div>
+        <div className="buttons">
+          <button onClick={() => {setValue(value + 1)}} className="increase">+</button>
+          <button onClick={() => {setValue(0)}} className="reset">Reset</button>
+          <button onClick={() => {setValue(value - 1)}} className="decrease">-</button>
+        </div>
       </div>
     </div>
   );
